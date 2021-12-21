54dbc6474ec22c774d1dd4a36d1cb44720065c25
gurkanozer <grknzr2609@gmail.com>
2021-12-21 13:44:57 +0200

    todo reducer export changed

--- src/context/todoReducer.js
+++ src/context/todoReducer.js
@@ -1,5 +1,5 @@
 import { ADD_TODO, CLEAR_COMPLETED, REMOVE_TODO, UPDATE_TODO, REORDER_TODOS } from './actionTypes';
-export default (state, action) => {
+const todoReducer (state, action) => {
     switch (action.type) {
         case ADD_TODO:
             return [
@@ -29,3 +29,4 @@ export default (state, action) => {
             return state;
     }
 }
+export default todoReducer;
